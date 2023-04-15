aa14e24507300455e5c3250d13ed31af8472531f
Kev <cloneofghosts@users.noreply.github.com>
2023-04-15 11:47:43 -0400

    Add Wind to Hourly Attributes

--- custom_components/pirateweather/weather.py
+++ custom_components/pirateweather/weather.py
@@ -363,6 +363,8 @@ class PirateWeather(WeatherEntity):
                         entry.d.get("precipIntensity"), 1
                     ),
                     ATTR_FORECAST_PRECIPITATION_PROBABILITY: entry.d.get("precipProbability")*100,
+                    ATTR_FORECAST_NATIVE_WIND_SPEED: entry.d.get("windSpeed"),
+                    ATTR_FORECAST_WIND_BEARING: entry.d.get("windBearing"),
                     ATTR_FORECAST_CONDITION: MAP_CONDITION.get(entry.d.get("icon")),
                 }
                 for entry in self._weather_coordinator.data.hourly().data
